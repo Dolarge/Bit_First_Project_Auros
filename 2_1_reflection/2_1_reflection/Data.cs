60829300b339812e242c2533eb525f283c5d044c
TRAVI-Yoon <yoonjae1595@gmail.com>
2021-03-30 17:28:16 +0900

    죄송함돠

---
+++ 2_1_reflection/2_1_reflection/Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1_reflection
+{
+    public class Si_new_Data
+    {
+        public string nm { get; set; }
+        public string n { get; set; }
+        public string k { get; set; }
+    }
+    class SiO2_new_Data
+    {
+        public string nm { get; set; }
+        public string n { get; set; }
+        public string k { get; set; }
+    }
+
+    class SiO2_1000nm_Data
+    {
+        public string nm { get; set; }
+        public string AOI { get; set; }
+        public string Psi { get; set; }
+        public string Delta { get; set; }
+    }
+}
