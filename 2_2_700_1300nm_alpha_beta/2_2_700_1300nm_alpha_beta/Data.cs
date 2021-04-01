7eeb7aa95b0af78b25cfa49ddb60ca158f3a6f62
TRAVI-Yoon <yoonjae1595@gmail.com>
2021-04-01 17:17:15 +0900

    세연이가 이해시킴

---
+++ 2_2_700_1300nm_alpha_beta/2_2_700_1300nm_alpha_beta/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2_700_1300nm_alpha_beta
+{
+    class SiO2_1000nm_Data
+    {
+        public string nm { get; set; }
+        public string AOI { get; set; }
+        public string alpha { get; set; }
+        public string beta { get; set; }
+    }
+    class SiO2_new_Data
+    {
+        public string nm { get; set; }
+        public string n { get; set; }
+        public string k { get; set; }
+    }
+    public class Si_new_Data
+    {
+        public string nm { get; set; }
+        public string n { get; set; }
+        public string k { get; set; }
+    }
+}
